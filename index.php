e4a4b34132233faaf1b4a095f825041e19640dfa
hockyyyy <119024992+hockyyyy@users.noreply.github.com>
2023-03-09 11:22:38 +0700

    chane 6 str

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
-    /* @var PDO $db */
-    $db = new PDO('mysql:host=localhost;dbname=posts', 'root');
+/* @var PDO $db */
+$db = new PDO('mysql:host=localhost;dbname=posts', 'root');
 
-    $limit_on_page = 3;
-    $get_page = isset($_GET['page']) ? $_GET['page'] * $limit_on_page : 0;
+$limit_on_page = 3;
+if ( isset($_GET['page']) ) { $get_page = $_GET['page'] * $limit_on_page; }
+else { $get_page = 0; }
 
-    $take_all_from_db = $db->query("SELECT * FROM post LIMIT {$get_page}, {$limit_on_page}")->fetchAll(PDO::FETCH_ASSOC);
+$take_all_from_db = $db->query("SELECT * FROM post LIMIT {$get_page}, {$limit_on_page}")->fetchAll(PDO::FETCH_ASSOC);
 
-    $take_count_query = $db->query('SELECT COUNT(body) FROM post')->fetch(PDO::FETCH_ASSOC);
-    $take_count_of_page = round(intdiv($take_count_query["COUNT(body)"]*10, $limit_on_page)/10, 0);
+$take_count_query = $db->query('SELECT COUNT(body) FROM post')->fetch(PDO::FETCH_ASSOC);
+$take_count_of_page = round(intdiv($take_count_query["COUNT(body)"]*10, $limit_on_page)/10, 0);
 ?>
 <!doctype html>
 <html lang="en">
@@ -21,12 +22,12 @@
 </head>
 <body>
 
-    <?php foreach($take_all_from_db as $item) : ?>
-        <div><?=$item['id']?> <?=$item['body']?></div>
-    <?php endforeach; ?>
+<?php foreach($take_all_from_db as $item) : ?>
+    <div><?=$item['id']?> <?=$item['body']?></div>
+<?php endforeach; ?>
 
-    <?php for($count = 0; $count <= $take_count_of_page-1; $count++) : ?>
-        <a href="?page=<?=$count?>"><?=$count+1?></a>
-    <?php endfor; ?>
+<?php for($count = 0; $count <= $take_count_of_page-1; $count++) : ?>
+    <a href="?page=<?=$count?>"><?=$count+1?></a>
+<?php endfor; ?>
 </body>
 </html>
